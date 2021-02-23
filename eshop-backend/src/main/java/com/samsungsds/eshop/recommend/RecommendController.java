aec6f184a21424f8fddb90b75450750057bd0ef6
ueong <dnfod@me.com>
2021-02-23 13:39:50 +0900

    changed api path to remove rewrite

--- eshop-backend/src/main/java/com/samsungsds/eshop/recommend/RecommendController.java
+++ eshop-backend/src/main/java/com/samsungsds/eshop/recommend/RecommendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/recommends")
+@RequestMapping(value="/api/recommends")
 public class RecommendController {
   private final RecommendService recommendService;
 
