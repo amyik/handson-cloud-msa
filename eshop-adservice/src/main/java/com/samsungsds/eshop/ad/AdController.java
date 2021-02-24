5171477c8076a269f56c2dd54bbdd9db1224f5d1
ueong <dnfod@me.com>
2021-02-24 06:35:03 +0000

    AdController fixed

--- eshop-adservice/src/main/java/com/samsungsds/eshop/ad/AdController.java
+++ eshop-adservice/src/main/java/com/samsungsds/eshop/ad/AdController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping(value="/api/ads")
